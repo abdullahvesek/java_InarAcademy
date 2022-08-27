88800bc97b5de346c8adfb7f63b56f3b3cce7cf4
abdullah vesek <vesekabdullah@gmail.com>
2022-08-27 16:49:20 +0300

    exercies

--- src/chapters_03/Exercise_02.java
+++ src/chapters_03/Exercise_02.java
@@ -1,10 +1,27 @@
 package chapters_03;
 
+import java.util.Scanner;
+
 public class Exercise_02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner (System.in);
 
+		
+		int digit_01 = (int)(Math.random() * 10);
+		int digit_02 = (int)(Math.random() * 10);
+		int digit_03 = (int)(Math.random() * 10);
+
+		// Prompt user to enter the sum of three integers
+		System.out.print(
+			"What is " + digit_01 + " + " + digit_02 + " + " + digit_03 + "? ");
+		
+		int answer = input.nextInt();
+		
+		System.out.println(
+			digit_01 + " + " + digit_02 + " + " + digit_03 + " = " + answer + " is " +
+			(digit_01 + digit_02 + digit_03 == answer));
 	}
-
 }
+
+
