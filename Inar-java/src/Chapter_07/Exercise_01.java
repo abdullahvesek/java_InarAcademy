266b606f8dce991aeea7cc968a1d77d19a0e4e45
abdullah vesek <vesekabdullah@gmail.com>
2022-09-28 21:55:29 +0300

    new

---
+++ Inar-java/src/Chapter_07/Exercise_01.java
@@ -0,0 +1,7 @@
+package Chapter_07;
+
+public class Exercise_01 {
+    public static void main(String[] args) {
+        System.out.println("hjhjhjhjhj");
+    }
+}
---
+++ src/chapters_07/Exercise_01.java
@@ -0,0 +1,7 @@
+package chapters_07;
+
+public class Exercise_01 {
+    public static void main(String[] args) {
+        System.out.println("gfgfgfgfg");
+    }
+}
