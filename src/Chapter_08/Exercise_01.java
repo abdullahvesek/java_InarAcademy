ccefdd5e14c2672058c6b4c62cc6fb1993b59ff6
abdullah <vesekabdullah@gmail.com>
2022-10-13 22:57:04 +0300

    bbbb

--- src/Chapter_08/Exercise_01.java
+++ src/Chapter_08/Exercise_01.java
@@ -3,7 +3,7 @@ package Chapter_08;
 public class Exercise_01 {
     public static void main(String[] args) {
         System.out.println("welcome");
-        System.out.println("dddd");
+        System.out.println("ddddşşş");
         System.out.println("aaaaggg");
     }
 }
